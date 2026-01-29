3aa2c6d181a31168cf7575c2b785c45e9f981234
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-01-29 13:38:57 -0300

    commit

---
+++ python/Conversor para Dolar.py
@@ -0,0 +1,6 @@
+N2 = 5.37
+
+print("Conversor para Dolar")
+
+N1 = float(input("Qual o dinheiro em real que voçe tem?"))
+print("O valor que voçe teria em dolares seria: ", N1 * N2)
---
+++ python/aventuras.py
@@ -0,0 +1,6 @@
+print('desafio')
+print('Caulculadora 10mil')
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite outro numero: '))
+tot = num1 + num2
+print('A soma de', num1 , 'e', num2 , 'é' , tot)
