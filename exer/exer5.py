d9783a59a1b3390bc1155e57708b8c1afb5ab133
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-01-31 14:31:24 -0300

    Adicionado mais medidas

--- exer/exer5.py
+++ exer/exer5.py
@@ -1,4 +1,15 @@
 metro = float(input('Digite seu metro: '))
+
+dm = metro * 10
 cm = metro * 100
 mm = metro * 1000
-print(f"Voçe colocou {metro} metros, tem {cm} centimetros e {mm} milimetros")
+dam = metro / 10
+hm = metro / 100
+km = metro / 1000
+
+print(f'A medida de {metro:.0f} metros é igual a {dm:.0f} decímetros')
+print(f'a medida de {metro:.0f} metros é igual a {cm:.0f} centrímetros')
+print(f'A medida de {metro:.0f} metros é igual a {mm:.0f} milimetros')
+print(f'A medida de {metro:.0f} metros é igual a {dam:.3f} decametros')
+print(f'A medida de {metro:.0f} metros é igual a {hm:.3f} hectômetros')
+print(f'A medida de {metro:.0f} metros é igual a {km:.3f} quilômetros')
