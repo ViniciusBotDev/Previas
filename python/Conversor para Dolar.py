d1c73bbea4d5fdc23f8bfa03d056bde387ece57d
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-01-31 15:22:39 -0300

    Simplificação no sistema de conversão

--- python/Conversor para Dolar.py
+++ python/Conversor para Dolar.py
@@ -1,6 +1,7 @@
 N2 = 5.37
 
 print("Conversor para Dolar")
-
-N1 = float(input("Qual o dinheiro em real que voçe tem?"))
-print("O valor que voçe teria em dolares seria: ", N1 * N2)
+print('=' * 50)
+N1 = float(input("Qual o dinheiro em real que voçe tem? "))
+print(f"O valor que voçe teria em dolares seria: {N1*N2}")
+print('=' * 50)
