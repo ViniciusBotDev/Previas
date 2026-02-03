fdaf320c6c875b1a242dffcbe5dd15ea76eaace5
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-03 15:49:11 -0300

    Atualiza cálculo de salário para incluir 15% de aumento e melhora a formatação da saída

--- exer/exer7.py
+++ exer/exer7.py
@@ -1,3 +1,5 @@
-sal = float(input("Digite o valor do seu salario: "))
-nsal = sal * 1.15
-print(f'O seu salario com 15% de aumento ficaria R${nsal}')
+salário = float(input("Digite o salário do funcionário: "))
+
+novo_salário = salário + (salário * 15 / 100)
+
+print(f'O salário do funcionário de R${salário:.2f}, com 15% de aumento ficará R${novo_salário:.2f}')
