2ab9dfb40ccde3c54ab55f8cd1720a36472fd3b7
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-03 15:45:04 -0300

    Corrige cálculo da área da parede para multiplicação de altura e largura

--- exer/exer6.py
+++ exer/exer6.py
@@ -1,5 +1,5 @@
 alt = int(input('Digite a altura da sua parede: '))
 larg = int(input('Digite a largura da sua parede: '))
-area = alt + larg
+area = alt * larg
 tinta = area / 2
-print(f'Sera preciso {tinta} latas de tinta para pintar a parede de sua area {area}')
+print(f'Sera preciso {tinta} latas de tinta para pintar a parede de área {area}m²')
