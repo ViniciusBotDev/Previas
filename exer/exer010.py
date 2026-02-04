5c2dba5fbf3bca5a3bbe84701a168de2cecc7623
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-03 22:09:50 -0300

    Melhora a formatação do cálculo do desconto no preço do produto

--- exer/exer010.py
+++ exer/exer010.py
@@ -1,5 +1,5 @@
 preço_produto = float(input('Digite o preço do produto: '))
 
-produto_descontado = preço_produto - (preço_produto*5/100)
+produto_descontado = preço_produto - (preço_produto * 5 / 100)
 
 print(f'Seu produto custa {preço_produto:.2f} mas com 5% de desconto ele irá ficar {produto_descontado:.2f}')
