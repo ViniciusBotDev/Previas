cf38b1443d162ce956a7d35b2a163f27259558d0
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-06 16:34:49 -0300

    Refatora importação da biblioteca math para usar apenas a função hypot

--- exer/exer09a.py
+++ exer/exer09a.py
@@ -1,7 +1,7 @@
-import math
+from math import hypot
 
 cat_ops = float(input('Digite um cateto:   '))
 cat_adj = float(input('Digite outro cateto:   '))
-hi = math.hypot(cat_ops, cat_adj)
+hi = hypot(cat_ops, cat_adj)
 
 print(f'A hipotenusa de {cat_ops} e {cat_adj} é {hi:.2f}')
