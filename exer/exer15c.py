f55a67ec6fe1d39a4e2d6369b5f9476fae375ce1
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-12 14:26:58 -0300

    Corrige a lógica de cálculo do novo salário para considerar corretamente os salários até R$1250

--- exer/exer15c.py
+++ exer/exer15c.py
@@ -1,6 +1,6 @@
 salario_atual = float(input('Digite o valor do salario: R$'))
-if salario_atual > 1250:
-    novo_salario = salario_atual + (salario_atual * 10 / 100)
-else:
+if salario_atual <= 1250:
     novo_salario = salario_atual + (salario_atual * 15 / 100)
+else:
+    novo_salario = salario_atual + (salario_atual * 10 / 100)
 print(f'O novo salario será de R${novo_salario:.2f}')
