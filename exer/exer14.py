07a6e9788384a43cdd9604d3bfd80adef4c3018e
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-12 14:00:59 -0300

    Adiciona loop para permitir tentativas contínuas na adivinhação do número

--- exer/exer14.py
+++ exer/exer14.py
@@ -1,9 +1,14 @@
 import random
+from time import sleep
 
 numero_aleatorio = random.randint(1, 5)
 print('Pensando em um número...')
-numero_escolhido = int(input('Escolha um número digitando entre 1 a 5 :  '))
-if numero_escolhido == numero_aleatorio:
-    print('Parabens voçe acertou o numero')
-else:
-    print(f'Errado o número certo era {numero_aleatorio}, voçe errou')
+while True:
+    numero_escolhido = int(input('Escolha um número digitando entre 1 a 5 :  '))
+    print('Verificando...')
+    sleep(2)
+    if numero_escolhido == numero_aleatorio:
+        print('Parabens voçe acertou o numero')
+    else:
+        print(f'Voçe errou o numero tente novamente')
+
