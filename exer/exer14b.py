f6e5b0c9981783643cce96b95f76fce3cbdc0af9
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-12 14:07:45 -0300

    Remove variável excessiva e simplifica cálculo da multa por velocidade

--- exer/exer14b.py
+++ exer/exer14b.py
@@ -1,7 +1,5 @@
 velocidade_atual = float(input('Que velocidade o carro está?  '))
 if velocidade_atual > 80:
-    excesso = velocidade_atual - 80
-    valor_multa = excesso * 7
+    valor_multa = (velocidade_atual-80) * 7
     print(f'Voçe foi multado no valor de {valor_multa:.2f} por ultrapassar 80kmh')
-else:
-    print('Boa viagem')
+print('Tenha um bom dia, dirija com segurança')
