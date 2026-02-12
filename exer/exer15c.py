25a00fa5a3c00560abac4113fbcb69f2e7d13f86
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-12 14:25:38 -0300

    Remover linhas em branco desnecessárias no resultado do cálculo salarial

--- exer/exer15c.py
+++ exer/exer15c.py
@@ -3,5 +3,4 @@ if salario_atual > 1250:
     novo_salario = salario_atual + (salario_atual * 10 / 100)
 else:
     novo_salario = salario_atual + (salario_atual * 15 / 100)
-
 print(f'O novo salario será de R${novo_salario:.2f}')
