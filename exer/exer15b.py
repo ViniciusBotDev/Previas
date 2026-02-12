a90381d0ef1e0bf4b2ef464e8bdc90ff9448d3e9
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-12 14:22:59 -0300

    Adiciona mensagens de espera antes de exibir o menor e maior valor digitado

--- exer/exer15b.py
+++ exer/exer15b.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 numero1 = int(input('Digite o primeiro numero:  '))
 numero2 = int(input('Digite o segundo numero:  '))
 numero3 = int(input('Digite o terceiro numero:  '))
@@ -13,6 +15,7 @@ if numero2 > numero1 and numero2 > numero3:
     maior = numero2
 if numero3 > numero2 and numero3 > numero1:
     maior = numero3
-
+print('Pensando..')
+sleep(1)
 print(f'O menor valor digitado foi: {menor}')
 print(f'O maior valor digitado foi: {maior}')
