fe93afa4822c19d799ea478aae253af8c83a4697
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-17 15:10:02 -0300

    Corrige a lógica de verificação de triângulo e aprimora a saída para tipos de triângulos

--- exer/exer15d.py
+++ exer/exer15d.py
@@ -1,8 +1,13 @@
 r1 = float(input('Primeiro segmento:  '))
 r2 = float(input('Segundo segmento:  '))
 r3 = float(input('Terceiro segmento:  '))
-
-if r1 < r2 * r3 and r2 < r1 + r3 and r3 < r1 + r2: 
-    print('Os segmentos de acima podem formar um triangulo!')
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2: 
+    print('Os segmentos de acima podem formar um triangulo ', end='')
+    if r1 == r2 == r3:
+        print('Equilátero')
+    if r1 != r2 != r3 != r3:
+        print('Escaleno')
+    else:
+        print('Isósceles')
 else:
     print('Os segmentos acima não podem formar um triangulo!')
