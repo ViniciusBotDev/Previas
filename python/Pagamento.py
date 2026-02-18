1dbea83a551bc1cb17a561f63f38f18ef0d17c86
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-18 15:12:59 -0300

    Refatora cálculo do valor final nas opções de pagamento

--- python/Pagamento.py
+++ python/Pagamento.py
@@ -7,18 +7,19 @@ Forma = int(input('''FORMAS DE PAGAMENTO
 [ 4 ] 3x ou mais no cartão
 Qual é a opção?  '''))
 if Forma == 1:
-    Valor_final = Preço_compras - (Preço_compras*10/100)
-    print(f'Sua compra de R${Preço_compras:.2f} ficará no valor final de R${Valor_final:.2f}')
+    total = Preço_compras - (Preço_compras*10/100)
+    print(f'Sua compra de R${Preço_compras:.2f} ficará no valor final de R${total:.2f}')
 elif Forma == 2:
-    Valor_final = Preço_compras - (Preço_compras*5/100)
-    print(f'Sua compra de R${Preço_compras:.2f} ficará no valor final de R${Valor_final:.2f}')
+    total = Preço_compras - (Preço_compras*5/100)
+    print(f'Sua compra de R${Preço_compras:.2f} ficará no valor final de R${total:.2f}')
 elif Forma == 3:
-    Valor_final = Preço_compras / 2
-    print(f'Sua comrpra de R${Preço_compras:.2f} ficará no valor final de 2x de R${Valor_final:.2f}')
+    total = Preço_compras / 2
+    print(f'Sua comrpra de R${Preço_compras:.2f} ficará no valor final de 2x de R${total:.2f}')
 elif Forma == 4:
     parcelas = int(input('Quantas parcelas?  '))
-    Valor_final = (Preço_compras + (Preço_compras*20/100)) / parcelas
-    print(f'Sua compra parcelada em {parcelas}x de R${Valor_final} COM JUROS')
-    print(f'Sua compra de R${Preço_compras:.2f} vai custar {(Preço_compras + (Preço_compras*20/100)):.2f}')
+    total = Preço_compras + (Preço_compras*20/100)
+    totparcelas = total / parcelas
+    print(f'Sua compra parcelada em {parcelas}x de R${totparcelas} COM JUROS')
+    print(f'Sua compra de R${Preço_compras:.2f} vai custar {total:.2f}')
 else:
     print('Opção inválida...')
