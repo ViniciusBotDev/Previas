1b5bf95fd19596f8ec2da9163ccae6b03101be5c
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-21 14:22:21 -0300

    Corrige a sintaxe de adição para soma e cont no script de múltiplos de 3

--- exer/exer45.py
+++ exer/exer45.py
@@ -3,6 +3,6 @@ cont = 0
 print('multiplos de 3')
 for n in range(1, 501, 2):
     if n % 3 == 0:
-        soma += n
-        cont += 1
+        soma = soma + n
+        cont = cont + 1
 print(f'a soma de todos os {cont} é {soma}')
