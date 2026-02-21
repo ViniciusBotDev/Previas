7610320bcfb7e4c4c173f548938aacc8c5b76da7
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-21 14:29:26 -0300

    Mudança na posição dos numeros

--- exer/exer46.py
+++ exer/exer46.py
@@ -2,4 +2,4 @@ num = int(input('Qual tabuada deseja ver?  '))
 vezes = int(input('Quantas vezes?  '))
 for c in range(1, vezes+1,):
     soma = num * c
-    print(f' {c} x {num} = {soma}')
+    print(f' {num} x {c} = {soma}')
