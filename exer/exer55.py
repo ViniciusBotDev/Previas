ea2662d3804f2359da975ea2e4e26dd987bb749b
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-21 20:08:16 -0300

    Treino de 'for'

---
+++ exer/exer55.py
@@ -0,0 +1,5 @@
+preços = [100, 250, 400]
+
+for valor in preços:
+    imposto = valor * 0.1  # Aqui eu não usei print, eu usei o item para calcular!
+    print(f"O imposto de R${valor} é R${imposto}")
