6fa22359e0d1c6d5990bc1289bccbeaae91ed1e9
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-02-26 13:57:20 -0300

    Sequência de Fibonacci

--- exer/exer59.py
+++ exer/exer59.py
@@ -1,4 +1,6 @@
+print('-'*30)
 print('Sequência de Fibonacci')
+print('-'*30)
 n = int(input('Quantos termos você quer mostrar? '))
 
 t1 = 0
@@ -9,9 +11,7 @@ cont = 3
 while cont <= n:
     t3 = t1 + t2
     print(f' → {t3}', end='')
-    
     t1 = t2 
     t2 = t3 
-    
     cont += 1
 print(' → FIM')
