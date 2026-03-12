2b08fde0bec1529dabb9880045adfc9cbdb456cb
ViniciusBotDev <vinicius.mmoreira3@gmail.com>
2026-03-12 14:43:54 -0300

    Mega sena sorteamento

---
+++ exer/exer83.py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+lista = []
+jogos = []
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont == 6:
+            break
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS', '-=' * 3)
+for i, l in enumerate(jogos):
+    sleep(0.6)
+    print(f'Jogo {i + 1}: {sorted(l)}')
